14cfb1ebb5b8f7f9ec3769641d45f85447364596
강승모 <seungmo.kang@musinsa.com>
2023-08-08 08:27:58 +0900

    system.out.println

--- src/main/java/com/seungmo/githubaction/GithubActionDemoApplication.java
+++ src/main/java/com/seungmo/githubaction/GithubActionDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubActionDemoApplication {
     public static void main(String[] args) {
+        System.out.println("==== ::: Github Action Demo ::: ====");
         SpringApplication.run(GithubActionDemoApplication.class, args);
     }
 }
